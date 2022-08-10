ca0590bfd9a7f98c84dc4d38ca2528043bd53d49
Manik <manikrana54@gmail.com>
2022-08-10 20:44:14 +0530

    Add LoadingState

--- part1/src/components/ResultTable.tsx
+++ part1/src/components/ResultTable.tsx
@@ -1,5 +1,11 @@
 import { useEffect, useState } from "react";
-import { createStyles, Table, ScrollArea } from "@mantine/core";
+import {
+  createStyles,
+  Table,
+  ScrollArea,
+  Pagination,
+  Loader,
+} from "@mantine/core";
 import axios from "axios";
 
 const useStyles = createStyles((theme) => ({
@@ -32,56 +38,88 @@ const useStyles = createStyles((theme) => ({
 export function ResultTable() {
   const { classes, cx } = useStyles();
   const [scrolled, setScrolled] = useState(false);
-  const [data, setData] = useState<any[]>([]);
+  const [data, setData] = useState<any[] | null>(null);
   const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [currentPage, setCurrentPage] = useState(1);
 
   useEffect(() => {
-    axios
-      .get(`https://api.github.com/repos/PHP-FFMpeg/PHP-FFMpeg/issues`)
-      .then((response) => {
+    const getData = async () => {
+      try {
+        const response = await axios.get(
+          `https://api.github.com/repos/PHP-FFMpeg/PHP-FFMpeg/issues?page=${currentPage}`
+        );
         setLoading(false);
         setData(response.data);
-        console.log(response.headers.link);
-      })
-      .catch((err) => {
+      } catch (err) {
         setError(err.message);
-        setData([]);
-      })
-      .finally(() => {
+        setData(null);
+      } finally {
         setLoading(false);
-      });
-  }, []);
+      }
+    };
+    getData();
+  }, [currentPage]);
+
+  const handlePageChagne = (page: number) => {
+    setCurrentPage(page);
+    setLoading(true);
+  };
 
   return (
-    <ScrollArea
-      sx={{
-        height: 600,
-        width: 1000,
-        borderRadius: "8px",
-        boxShadow: "rgba(0, 0, 0, 0.16) 0px 1px 4px",
-      }}
-      onScrollPositionChange={({ y }) => setScrolled(y !== 0)}
-    >
-      <Table sx={{ minWidth: 700 }}>
-        <thead className={cx(classes.header, { [classes.scrolled]: scrolled })}>
-          <tr>
-            <th>No.</th>
-            <th>Issue</th>
-            <th>Posted By</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data &&
-            data.map((row) => (
-              <tr key={row.id}>
-                <td>{row.number}</td>
-                <td>{row.title}</td>
-                <td>{row.user.login}</td>
+    <div>
+      <ScrollArea
+        sx={{
+          height: 600,
+          width: 1000,
+          borderRadius: "8px",
+          boxShadow: "rgba(0, 0, 0, 0.16) 0px 1px 4px",
+        }}
+        onScrollPositionChange={({ y }) => setScrolled(y !== 0)}
+      >
+        <Table sx={{ minWidth: 700, minHeight: 500 }}>
+          <thead
+            className={cx(classes.header, { [classes.scrolled]: scrolled })}
+          >
+            <tr>
+              <th>No.</th>
+              <th>Issue</th>
+              <th>Posted By</th>
+            </tr>
+          </thead>
+          {loading ? (
+            // display a loading indicator in the middle of the table
+            <tbody>
+              <tr>
+                <td colSpan={3}>
+                  <div
+                    style={{
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center",
+                      height: "100%",
+                    }}
+                  >
+                    <Loader size="lg" />
+                  </div>
+                </td>
               </tr>
-            ))}
-        </tbody>
-      </Table>
-    </ScrollArea>
+            </tbody>
+          ) : (
+            <tbody>
+              {data &&
+                data.map((row) => (
+                  <tr key={row.id}>
+                    <td>{row.number}</td>
+                    <td>{row.title}</td>
+                    <td>{row.user.login}</td>
+                  </tr>
+                ))}
+            </tbody>
+          )}
+        </Table>
+      </ScrollArea>
+      <Pagination page={currentPage} onChange={handlePageChagne} total={10} />
+    </div>
   );
 }
